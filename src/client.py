d55953ab2b32935ef9da45cee901e4e0265bf7f8
David Fornas <davidfornas@gmail.com>
2015-02-06 13:57:48 +0100

    Service test clients finished. Demo navigation through topics added.

--- src/client.py
+++ src/client.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
-
 from arm5e_arm_services.srv import GetJoints
 from arm5e_arm_services.srv import ForwardKinematics
 from arm5e_arm_services.srv import InverseKinematics
 from arm5e_arm_services.srv import MoveArm
 from pcl_manipulation.srv import DetectObject
-#from beginner_tutorials.srv import *
 import rospy
 
+# TO-DO: Object Oriented version
+
+#Get current joint state (position)
 def callGetJoints():
 	rospy.wait_for_service('get_joints')
 	get_joints = rospy.ServiceProxy('get_joints', GetJoints)
 	try:
 		res = get_joints()
-		print("Joint positions" + str(res.joints.position))
+		#print("Joint positions" + str(res.joints.position))
 		return res.joints.position
 	except rospy.ServiceException as exc:
 		print("Service did not process request: " + str(exc))
 		
+#Call Direct Kinemaics joints=>result_pose	
 def callFK(j):
 	rospy.wait_for_service('forward_kinematics')
 	forward_kinematics = rospy.ServiceProxy('forward_kinematics', ForwardKinematics)
@@ -28,16 +30,18 @@ def callFK(j):
 	except rospy.ServiceException as exc:
 		print("Service did not process request: " + str(exc))
 		
-		
+#Call Inverse Kinemaics pose=>joints,result_pose	
 def callIK( req ):
 	rospy.wait_for_service('inverse_kinematics')
 	inverse_kinematics = rospy.ServiceProxy('inverse_kinematics', InverseKinematics)
 	try:
 		res = inverse_kinematics(req)
 		print("Joint positions for desired bMe: " + str(res))
+		return res
 	except rospy.ServiceException as exc:
 		print("Service did not process request: " + str(exc))
-				
+		
+#Move arm at a certain joint speed		
 def callMoveArm( vel ):
 	rospy.wait_for_service('move_arm')
 	move_arm = rospy.ServiceProxy('move_arm', MoveArm)
@@ -47,28 +51,42 @@ def callMoveArm( vel ):
 	except rospy.ServiceException as exc:
 		print("Service did not process request: " + str(exc))		
 
+#Move arm to a certain joint configuration
+def callMoveArmTo( position ):
+	cj=callGetJoints()
+	spd=[position[0]-cj[0],position[1]-cj[1],position[2]-cj[2],position[3]-cj[3],position[4]-cj[4]]
+	d=(position[0]-cj[0])**2+(position[1]-cj[1])**2+(position[2]-cj[2])**2+(position[3]-cj[3])**2+(position[4]-cj[4])**2
+	while d>=0.05:
+		cj=callGetJoints()
+		spd=[position[0]-cj[0],position[1]-cj[1],position[2]-cj[2],position[3]-cj[3],position[4]-cj[4]]
+		d=(position[0]-cj[0])**2+(position[1]-cj[1])**2+(position[2]-cj[2])**2+(position[3]-cj[3])**2+(position[4]-cj[4])**2
+		callMoveArm(spd)
 	
+#Use object detection to retrieve object position
 def callDetectObject():
 	rospy.wait_for_service('detect_object')
 	detect_object = rospy.ServiceProxy('detect_object', DetectObject)
 	try:
-		res = detect_object()
-		print("Object position:")
+		# Known parameters, must allow change and tunning.
+		res = detect_object(0.3,1.57,0) 
+		print("Object position:" + str(res))
 		return res
 	except rospy.ServiceException as exc:
-		print("Service did not process request: " + str(exc))		
-			
-		
+		print("Service did not process request: " + str(exc))				
 	
 print("Lets test some services...: ")
+
+#Move ARM
 #while 1<2:
 #	callMoveArm([-0.1,-0.1,0,0,0])
 
-j=callGetJoints()
-b=callFK(j)
-callIK(b)
-callDetectObject()
-#while 1<2:
-#	
-#	
-#	callMoveArm([0.1,0.1,0,0,0])
+#Test Kinematics
+#j=callGetJoints()
+#b=callFK(j)
+#callIK(b)
+
+#Object detection
+res=callDetectObject()
+res2=callIK(res.cMo)
+bMe=callFK(res2.out_joints)
+callMoveArmTo(res2.out_joints)
---
+++ src/uwSimInt.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+
+# TO-DO: Object Oriented version
+# Remove globals
+pub=[]
+dat=[]
+ok=False
+
+#Navigation message callback.
+def navCallback(data):
+    global dat
+    p=data.pose.pose.position
+    if(not(p.x==0 and p.y==0 and p.z==0)):
+		dat=data
+		global ok
+		ok=True
+
+#Send speed to vehicle during t seconds.
+def sendSpeed(x, y, z, yaw, t):
+	global pub
+	pub = rospy.Publisher('/dataNavigator', Odometry, queue_size=10)
+	rate = rospy.Rate(25)
+	i=0 
+	while i<25*t:
+		nav=Odometry()
+		nav.twist.twist.linear.x=x
+		nav.twist.twist.linear.y=y
+		nav.twist.twist.linear.z=z
+		nav.twist.twist.angular.z=yaw
+		pub.publish(nav)
+		rate.sleep()
+		i+=1		
+
+#Using Odometry, got to position sending speeds.
+def sendPosition(x, y, z):
+	global sub
+	sub = rospy.Subscriber("/dataNavigator", Odometry, navCallback)
+	while not ok:
+		a=0	
+	p=dat.pose.pose.position
+	dx = x-p.x
+	dy = y-p.y
+	dz = z-p.z
+	rate = rospy.Rate(15)
+	while dx**2+dy**2+dz**2>0.2:
+		sendSpeed(dx/3., dy/3., dz/3., 0, 0.2)
+		rate.sleep()
+		p=dat.pose.pose.position
+		dx = x-p.x
+		dy = y-p.y
+		dz = z-p.z
+	sub.unregister()
+
+if __name__ == '__main__':
+    try:
+		rospy.init_node('navigator', anonymous=True)
+		sendSpeed(0.6,0.6,0.1,0.3,3)
+		sendPosition(3,3,3)
+		rospy.spin()        
+    except rospy.ROSInterruptException:
+        pass
